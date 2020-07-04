de2649ebb847f18ce51b8ffaabe894b1c08de563
Dhaval Soneji <dhaval@soneji.xyz>
2020-07-05 00:59:44 +0100

    fix url tings in player.js

--- bbd/player.js
+++ bbd/player.js
@@ -10,24 +10,22 @@ String.prototype.replaceAll = function (stringToFind, stringToReplace) {
   return temp;
 };
 (function () {
-
-
   var DEFAULT_MANIFEST = "manifest.json";
   var current_url_params = new URLSearchParams(location.href);
 
   if (current_url_params.has("href")) {
-    console.log("Found manifest in params")
+    console.log("Found manifest in params");
     var manifest_url = current_url_params.get("href");
   } else {
     var manifest_url = DEFAULT_MANIFEST;
-  };
+  }
 
   if (current_url_params.has("track")) {
-    console.log("Found reference to a document in params")
+    console.log("Found reference to a document in params");
     var track = current_url_params.get("track");
   } else {
     var track = undefined;
-  };
+  }
 
   var audio = document.getElementById("audio-element");
   var audio_source = document.getElementById("audio-source");
@@ -39,11 +37,16 @@ String.prototype.replaceAll = function (stringToFind, stringToReplace) {
   var saved_track = localStorage.getItem(manifest_url + "#track");
   var saved_position = localStorage.getItem(manifest_url + "#t");
   if (saved_position && saved_track) {
-    console.log("Found previous position at: " + saved_track + "#t=" + saved_position)
-    initializeNavigation(manifest_url, saved_track).then(
-      function () { audio.currentTime = saved_position }).catch(function () { });
+    console.log(
+      "Found previous position at: " + saved_track + "#t=" + saved_position
+    );
+    initializeNavigation(manifest_url, saved_track)
+      .then(function () {
+        audio.currentTime = saved_position;
+      })
+      .catch(function () {});
   } else {
-    initializeNavigation(manifest_url, track).catch(function () { });
+    initializeNavigation(manifest_url, track).catch(function () {});
   }
 
   audio.addEventListener("timeupdate", function () {
@@ -57,108 +60,119 @@ String.prototype.replaceAll = function (stringToFind, stringToReplace) {
       updateTrack(manifest_url, next.href).then(function () {
         audio.play();
       });
-    };
+    }
   });
 
   next.addEventListener("click", function (event) {
     if (next.hasAttribute("href")) {
       updateTrack(manifest_url, next.href).then(function () {
-        audio.play()
+        audio.play();
       });
-    };
+    }
     event.preventDefault();
   });
 
   previous.addEventListener("click", function (event) {
     if (previous.hasAttribute("href")) {
       updateTrack(manifest_url, previous.href).then(function () {
-        audio.play()
+        audio.play();
       });
-    };
+    }
     event.preventDefault();
   });
 
   function getManifest(url) {
-    return fetch(url).catch(function () {
-      return caches.match(url);
-    }).then(function (response) {
-      return response.json();
-    })
-  };
+    return fetch(url)
+      .catch(function () {
+        return caches.match(url);
+      })
+      .then(function (response) {
+        return response.json();
+      });
+  }
 
   function initializeNavigation(url, track_url) {
-    return getManifest(url).then(function (json) {
-      var title = json.metadata.title;
-      console.log("Title of the publication: " + title);
-      document.querySelector("title").textContent = title;
+    return getManifest(url)
+      .then(function (json) {
+        var title = json.metadata.title;
+        console.log("Title of the publication: " + title);
+        document.querySelector("title").textContent = title;
 
-      //Search for cover and add it
-      if (json.links) {
-        json.links.forEach(function (link) {
-          if (link.rel) {
-            if (link.rel == "cover") {
-              console.log("Found cover: " + link.href);
-              // cover.src = new URL(link.href, url).href; // doesnt work coz default manifest is not a url
-              cover.src = new URL(link.href).href;
+        //Search for cover and add it
+        if (json.links) {
+          json.links.forEach(function (link) {
+            if (link.rel) {
+              if (link.rel == "cover") {
+                console.log("Found cover: " + link.href);
+                // cover.src = new URL(link.href, url).href; // doesnt work coz default manifest is not a url
+                cover.src = new URL(link.href).href;
+              }
             }
-          }
-        }, this);
-      };
+          }, this);
+        }
 
-      return json.readingOrder;
-    }).then(function (readingOrder) {
+        return json.readingOrder;
+      })
+      .then(function (readingOrder) {
+        //Set start track
+        // var start_url = new URL(readingOrder[0].href, url).href;
+        var start_url = new URL(readingOrder[0].href).href;
 
-      //Set start track
-      var start_url = new URL(readingOrder[0].href, url).href;
-
-      if (track_url) {
-        updateTrack(url, track_url);
-      } else {
-        updateTrack(url, start_url);
-      }
-
-    });
-  };
+        if (track_url) {
+          updateTrack(url, track_url);
+        } else {
+          updateTrack(url, start_url);
+        }
+      });
+  }
 
   function updateTrack(url, current) {
-    console.log("Getting " + url)
+    console.log("Getting " + url);
     if (current) {
       var current_src = current;
     } else {
       var current_src = audio_source.src;
     }
-    return getManifest(url).then(function (json) { return json.readingOrder }).then(function (readingOrder) {
-
-      var current_index = readingOrder.findIndex(function (element) {
-        var element_url = new URL(element.href, url);
-        return element_url.href == current_src;
+    return getManifest(url)
+      .then(function (json) {
+        return json.readingOrder;
       })
-      temp = readingOrder[current_index].href;
-      temp = temp.substring(5, temp.length-4);
-      temp = temp.replaceAll("_"," ")
-      ting.innerHTML = temp;
-      if (current_index >= 0) {
+      .then(function (readingOrder) {
+        var current_index = readingOrder.findIndex(function (element) {
+          var element_url = new URL(element.href); //, url);
+          return element_url.href == current_src;
+        });
+        temp = readingOrder[current_index].href;
+        temp = temp.substring(5, temp.length - 4);
+        temp = temp.replaceAll("_", " ");
+        ting.innerHTML = temp;
+        if (current_index >= 0) {
+          // audio_source.src = new URL(readingOrder[current_index].href, url).href;
+          audio_source.src = new URL(readingOrder[current_index].href).href;
+          localStorage.setItem(url + "#track", audio_source.src);
+          audio_source.type = readingOrder[current_index].type;
+          audio.load();
 
-        audio_source.src = new URL(readingOrder[current_index].href, url).href;
-        localStorage.setItem(url + "#track", audio_source.src);
-        audio_source.type = readingOrder[current_index].type;
-        audio.load();
+          if (current_index > 0) {
+            console.log(
+              "Previous track is: " + readingOrder[current_index - 1].href
+            );
+            // previous.href = new URL(readingOrder[current_index - 1].href, url).href;
+            previous.href = new URL(readingOrder[current_index - 1].href).href;
+          } else {
+            previous.removeAttribute("href");
+          }
 
-        if (current_index > 0) {
-          console.log("Previous track is: " + readingOrder[current_index - 1].href);
-          previous.href = new URL(readingOrder[current_index - 1].href, url).href;
-        } else {
-          previous.removeAttribute("href");
-        };
-
-        if (current_index < (readingOrder.length - 1)) {
-          console.log("Next track is: " + readingOrder[current_index + 1].href);
-          next.href = new URL(readingOrder[current_index + 1].href, url).href;
-        } else {
-          next.removeAttribute("href");
-        };
-      }
-    });
-  };
-
-}());
+          if (current_index < readingOrder.length - 1) {
+            console.log(
+              "Next track is: " + readingOrder[current_index + 1].href
+            );
+            next.href = new URL(readingOrder[current_index + 1].href).href;
+            next.href = new URL(readingOrder[current_index + 1].href, url).href;
+          } else {
+            next.removeAttribute("href");
+          }
+        }
+      });
+  }
+})();
